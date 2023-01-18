0ce8435e3468d9fd926909710a9708d8a1ded05e
Olumide Fadahunsi <oli2tup@gmail.com>
2023-01-18 13:23:15 +0000

    first commit

---
+++ test.py
@@ -0,0 +1,9 @@
+ #--------------------------------------------------------------#
+# Making a donwloadable calulcator with GUI using Python 
+# Basic project
+
+# Making the GUI 
+# programming the buttons 
+# make it downloadable
+ #--------------------------------------------------------------#
+ 
